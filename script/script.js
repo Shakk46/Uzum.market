d4fcda6943c6a09e10e7dbebd4a839736321a620
Shakk46 <shaxrik46@gmail.com>
2023-04-27 13:39:08 +0500

    Added the responsive search-bar.

--- script/script.js
+++ script/script.js
@@ -9,3 +9,24 @@ function createDOMElement(type, className, textCont) {
     element.className = className
     element.textContent = textCont
 }
+
+// Добавление анимации бургера
+const burger = doc.querySelector('.burger')
+burger.addEventListener('click', function(){
+    doc.querySelector('.burger span').classList.toggle('active');
+})
+
+// Анимация расширения поиска
+const searchBarAdapt = doc.querySelector('.search-bar-adapt')
+const searchBarInput = doc.querySelector('.search-bar-adapt input')
+const searchBarClose = doc.querySelector('.search__close')
+
+searchBarInput.addEventListener('click', () => {
+    searchBarAdapt.classList.add('search-bar-adapt__focus')
+    searchBarClose.style = 'display:block'
+})
+
+searchBarClose.addEventListener('click', () => {
+    searchBarAdapt.classList.remove('search-bar-adapt__focus')
+    searchBarClose.style = 'display:none;'
+})
